51f87048a83cee379813d38e8cdda52bb71d8797
User <User@DESKTOP-2IEMKP3>
2025-06-07 23:17:05 -0500

    mi aporte es la impresion pizzeria

---
+++ src/tf_grupo08/TF_Grupo08.java
@@ -0,0 +1,11 @@
+
+package tf_grupo08;
+public class TF_Grupo08 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("pizzeria");
+    }
+    
+    
+}
