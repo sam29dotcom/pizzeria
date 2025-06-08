ea0f749edb00ebf9c7aa4a1a34dc261327c855b8
User <User@DESKTOP-2IEMKP3>
2025-06-07 23:52:45 -0500

    puse pizzeria2

--- src/tf_grupo08/TF_Grupo08.java
+++ src/tf_grupo08/TF_Grupo08.java
@@ -4,7 +4,7 @@ public class TF_Grupo08 {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("pizzeria");
+        System.out.println("pizzeria2");
     }
     
     
