388d82259c3c93d7b7b5f648a11d69bf72d1d8b2
User <User@DESKTOP-2IEMKP3>
2025-06-20 17:46:10 -0500

    estoy avanzando

---
+++ src/tf_grupo08/Usuario.java
@@ -0,0 +1,20 @@
+package tf_grupo08;
+public class Usuario {
+    private int dni;
+    private String nombre;
+    private String apellido;
+    private String domicilio;
+    private String telefono;
+    private String nUsuario;
+    
+    //Método
+        public void saludarUsuario() {
+            System.out.println("Bienvenido a Pizzeria Mix&Try");
+}
+    //Método    
+        public void verificarNombre(){
+        
+        
+        
+        }
+}
